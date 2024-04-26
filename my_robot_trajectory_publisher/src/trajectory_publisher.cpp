155aec22c03ba23d481a650b2dca24a71d5bf559
Shashwat Mudugur Ashok Kumar <77918071+Shashwat1524@users.noreply.github.com>
2024-04-27 00:36:21 +0530

    Update trajectory_publisher.cpp

--- my_robot_trajectory_publisher/src/trajectory_publisher.cpp
+++ my_robot_trajectory_publisher/src/trajectory_publisher.cpp
@@ -5,52 +5,76 @@
 #include "visualization_msgs/msg/marker_array.hpp"
 #include "anscer_interface/srv/trajectory_path.hpp"
 #include <chrono>
-#include <vector>
-#include <string>
 #include <fstream>
-#include <chrono> 
-#include <thread>
-
 
 using namespace std::chrono_literals;
-using std::placeholders::_1;
-using std::placeholders::_2;
-
-
 
 class TrajectoryPublisherSaver : public rclcpp::Node
 {
+private:
+    static bool recording_started;
+    static std::chrono::time_point<std::chrono::steady_clock> start_time;
+    static std::chrono::seconds duration;
+    static std::ofstream csv_file;
+
 public:
     TrajectoryPublisherSaver() : Node("trajectory_publisher_saver")
     {
         odometry_subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
             "odom", 10, std::bind(&TrajectoryPublisherSaver::odometry_callback, this, std::placeholders::_1));
-        RCLCPP_INFO(this->get_logger(),"Subscribing to Odometry Data!");
+        RCLCPP_INFO(this->get_logger(), "Subscribing to Odometry Data!");
 
         trajectory_publisher_ = this->create_publisher
         <visualization_msgs::msg::MarkerArray>("trajectory_marker_publisher_test", 10);
-        RCLCPP_INFO(this->get_logger(),"Publishing Marker Data!");
-    
-        trajectory_server_ =this->create_service<anscer_interface::srv::TrajectoryPath>("trajectory_service_test",std::bind(&TrajectoryPublisherSaver::callbacksaver, this ,_1,_2));
-        RCLCPP_INFO(this->get_logger(),"Service has Started!");
+        RCLCPP_INFO(this->get_logger(), "Publishing Marker Data!");
+
+        trajectory_server_ = this->create_service
+        <anscer_interface::srv::TrajectoryPath>("trajectory_service_test", std::bind(&TrajectoryPublisherSaver::callbacksaver, this, std::placeholders::_1, std::placeholders::_2));
+        RCLCPP_INFO(this->get_logger(), "Service has Started!");
     }
 
-
 private:
-    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr trajectory_publisher_;
-    rclcpp::Service<anscer_interface::srv::TrajectoryPath>::SharedPtr trajectory_server_;
+    rclcpp::Publisher
+    <visualization_msgs::msg::MarkerArray>::SharedPtr trajectory_publisher_;
+    rclcpp::Service
+    <anscer_interface::srv::TrajectoryPath>::SharedPtr trajectory_server_;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odometry_subscription_;
 
-    std::vector<geometry_msgs::msg::Pose> trajectory_;
-    visualization_msgs::msg::MarkerArray marker_array;
+    std::vector
+    <geometry_msgs::msg::Pose> trajectory_;
 
-    geometry_msgs::msg::Pose createPose(double x, double y, double z)
+    void odometry_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
     {
-        geometry_msgs::msg::Pose pose;
-        pose.position.x = x;
-        pose.position.y = y;
-        pose.position.z = z;
-        return pose;
+        trajectory_.push_back(msg->pose.pose);
+        publish_trajectory();
+        
+        if (recording_started && std::chrono::steady_clock::now() >= start_time + duration)
+        {
+            recording_started = false;
+            csv_file.close();
+            RCLCPP_INFO(this->get_logger(), "Recording Stopped!");
+            return;
+        }
+
+        if (!recording_started)
+        {
+            return;
+        }
+
+
+        if (!csv_file.is_open())
+        {
+            RCLCPP_ERROR(this->get_logger(), "CSV file is not open");
+            return;
+        }
+
+        csv_file << msg->pose.pose.position.x << ","
+                 << msg->pose.pose.position.y << ","
+                 << msg->pose.pose.position.z << ","
+                 << msg->pose.pose.orientation.x << ","
+                 << msg->pose.pose.orientation.y << ","
+                 << msg->pose.pose.orientation.z << ","
+                 << msg->pose.pose.orientation.w << "\n";
     }
 
     void publish_trajectory()
@@ -80,63 +104,33 @@ private:
         trajectory_publisher_->publish(marker_array);
     }
 
-    void odometry_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
-    {
-        trajectory_.push_back(msg->pose.pose);
-        publish_trajectory();
-    }
-
     void callbacksaver(const anscer_interface::srv::TrajectoryPath::Request::SharedPtr request,
-                   const anscer_interface::srv::TrajectoryPath::Response::SharedPtr response)
+                       const anscer_interface::srv::TrajectoryPath::Response::SharedPtr response)
     {
-        std::ofstream csv_file(request->file_name + ".csv");
+        csv_file.open(request->file_name + ".csv", std::ios::app);
 
         if (csv_file.is_open())
         {
-            csv_file << "PositionX,PositionY,PositionZ,OrientationX,OrientationY,OrientationZ,OrientationW\n";
+            start_time = std::chrono::steady_clock::now();
+            duration = std::chrono::seconds(request->duration);
+            recording_started = true;
 
-            auto start_time = std::chrono::steady_clock::now();
-            auto end_time = start_time + std::chrono::seconds(request->duration);
-            auto sample_duration = std::chrono::milliseconds(10);
-
-            RCLCPP_INFO(this->get_logger(), "Recording Trajectory Data for %ld seconds", request->duration);
-
-            
-            while (std::chrono::steady_clock::now() < end_time )
-            {
-                
-                for (const auto &pose : trajectory_)
-                {
-                    auto current_time = std::chrono::steady_clock::now();
-                    if (current_time >= end_time)
-                        break;
-                    
-                    csv_file << pose.position.x << ","
-                            << pose.position.y << ","
-                            << pose.position.z << ",";
-
-                    
-                    csv_file << pose.orientation.x << ","
-                            << pose.orientation.y << ","
-                            << pose.orientation.z << ","
-                            << pose.orientation.w << "\n";
-
-                    std::this_thread::sleep_for(sample_duration);
-                }
-            }
-
-            csv_file.close();
-            RCLCPP_INFO(this->get_logger(), "CSV file Created Successfully with File Name %s.csv", request->file_name.c_str());
+            RCLCPP_INFO(this->get_logger(), "File Name: %s.csv", request->file_name.c_str());
+            RCLCPP_INFO(this->get_logger(), "Recording started for %ld seconds",request->duration);
             response->success = true;
         }
         else
         {
-            RCLCPP_ERROR(this->get_logger(), "Failed to create CSV file: %s.csv due to Error", request->file_name.c_str());
+            RCLCPP_ERROR(this->get_logger(), "Failed to open CSV file for writing: %s", request->file_name.c_str());
             response->success = false;
         }
     }
-
 };
+
+bool TrajectoryPublisherSaver::recording_started = false;
+std::chrono::time_point<std::chrono::steady_clock> TrajectoryPublisherSaver::start_time;
+std::chrono::seconds TrajectoryPublisherSaver::duration;
+std::ofstream TrajectoryPublisherSaver::csv_file;
 
 int main(int argc, char *argv[])
 {
@@ -146,4 +140,3 @@ int main(int argc, char *argv[])
     rclcpp::shutdown();
     return 0;
 }
-
